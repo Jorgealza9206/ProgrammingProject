a7aba65464255ff34abdcf78f4a9dfe1901e7334
Jorgealza9206 <jaalzar@correo.udistrital.edu.co>
2022-04-11 15:04:14 -0500

    Previo a probar comunicacion cifrada

--- MaquinaNativa2/Receptor.py
+++ MaquinaNativa2/Receptor.py
@@ -85,7 +85,7 @@ class Receptor(gr.top_block, Qt.QWidget):
         )
         self.rtlsdr_source_0.set_time_unknown_pps(osmosdr.time_spec_t())
         self.rtlsdr_source_0.set_sample_rate(samp_rate)
-        self.rtlsdr_source_0.set_center_freq(87.5e6, 0)
+        self.rtlsdr_source_0.set_center_freq(89.9e6, 0)
         self.rtlsdr_source_0.set_freq_corr(0, 0)
         self.rtlsdr_source_0.set_dc_offset_mode(0, 0)
         self.rtlsdr_source_0.set_iq_balance_mode(0, 0)
