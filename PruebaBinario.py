0e2d0083455215a2cf17f1c79e6fd10f7767455c
Jorgealza9206 <jaalzarcorreo.udistrital.edu.co>
2022-04-17 19:35:17 -0500

    Instalando pycryptodome en PC de Julián, buscando como ejecutar con try except el script de desencripción y diagnósticar como se guardan los binarios

--- PruebaBinario.py
+++ PruebaBinario.py
@@ -26,6 +26,7 @@ from gnuradio.filter import firdes
 import sip
 from gnuradio import audio
 from gnuradio import blocks
+import pmt
 from gnuradio import gr
 import sys
 import signal
@@ -36,7 +37,7 @@ from gnuradio.qtgui import Range, RangeWidget
 
 from gnuradio import qtgui
 
-class Prueba_1(gr.top_block, Qt.QWidget):
+class PruebaBinario(gr.top_block, Qt.QWidget):
 
     def __init__(self):
         gr.top_block.__init__(self, "Not titled yet")
@@ -59,7 +60,7 @@ class Prueba_1(gr.top_block, Qt.QWidget):
         self.top_grid_layout = Qt.QGridLayout()
         self.top_layout.addLayout(self.top_grid_layout)
 
-        self.settings = Qt.QSettings("GNU Radio", "Prueba_1")
+        self.settings = Qt.QSettings("GNU Radio", "PruebaBinario")
 
         try:
             if StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
@@ -173,10 +174,9 @@ class Prueba_1(gr.top_block, Qt.QWidget):
         self._freq_range = Range(20, 20000, 10, 500, 200)
         self._freq_win = RangeWidget(self._freq_range, self.set_freq, 'freq', "counter_slider", float)
         self.top_grid_layout.addWidget(self._freq_win)
-        self.blocks_wavfile_source_0 = blocks.wavfile_source('C:\\Users\\Julian\\Desktop\\ProgrammingProject\\Melendi - Destino o Casualidad ft. Ha Ash VDownloader.wav', False)
         self.blocks_multiply_const_vxx_0 = blocks.multiply_const_ff(volumen)
-        self.blocks_file_sink_0 = blocks.file_sink(gr.sizeof_float*1, 'C:\\Users\\Julian\\Desktop\\ProgrammingProject\\sample_1.bin', False)
-        self.blocks_file_sink_0.set_unbuffered(False)
+        self.blocks_file_source_0 = blocks.file_source(gr.sizeof_float*1, 'C:\\Users\\Julian\\Desktop\\ProgrammingProject\\sample_1.bin', False, 0, 0)
+        self.blocks_file_source_0.set_begin_tag(pmt.PMT_NIL)
         self.audio_sink_0 = audio.sink(samp_rate, '', True)
 
 
@@ -184,15 +184,14 @@ class Prueba_1(gr.top_block, Qt.QWidget):
         ##################################################
         # Connections
         ##################################################
+        self.connect((self.blocks_file_source_0, 0), (self.blocks_multiply_const_vxx_0, 0))
+        self.connect((self.blocks_file_source_0, 0), (self.qtgui_freq_sink_x_0, 0))
+        self.connect((self.blocks_file_source_0, 0), (self.qtgui_time_sink_x_0, 0))
         self.connect((self.blocks_multiply_const_vxx_0, 0), (self.audio_sink_0, 0))
-        self.connect((self.blocks_wavfile_source_0, 0), (self.blocks_file_sink_0, 0))
-        self.connect((self.blocks_wavfile_source_0, 0), (self.blocks_multiply_const_vxx_0, 0))
-        self.connect((self.blocks_wavfile_source_0, 0), (self.qtgui_freq_sink_x_0, 0))
-        self.connect((self.blocks_wavfile_source_0, 0), (self.qtgui_time_sink_x_0, 0))
 
 
     def closeEvent(self, event):
-        self.settings = Qt.QSettings("GNU Radio", "Prueba_1")
+        self.settings = Qt.QSettings("GNU Radio", "PruebaBinario")
         self.settings.setValue("geometry", self.saveGeometry())
         event.accept()
 
@@ -221,7 +220,7 @@ class Prueba_1(gr.top_block, Qt.QWidget):
 
 
 
-def main(top_block_cls=Prueba_1, options=None):
+def main(top_block_cls=PruebaBinario, options=None):
 
     if StrictVersion("4.5.0") <= StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
         style = gr.prefs().get_string('qtgui', 'style', 'raster')
