6a02ac3d3c635de46281fbc1c5342ad38124e18f
Jorgealza9206 <jaalzarcorreo.udistrital.edu.co>
2022-04-17 21:50:32 -0500

    commit para cambiara a rama Receptor

--- MaquinaNativa2/RSA_docPythonDES.py
+++ MaquinaNativa2/RSA_docPythonDES.py
@@ -1,21 +1,27 @@
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import AES, PKCS1_OAEP
 
-file_in = open("encrypted_data.wav", "rb")
-file_out = open("sample_2.bin", "wb")
+while True:
+   try:
+      file_in = open("encrypted_data.bin", "rb")
+      file_out = open("sample_2.bin", "wb")
 
-private_key = RSA.import_key(open("private.pem").read()) #Importa la llave privada
+      private_key = RSA.import_key(open("private.pem").read()) #Importa la llave privada
 
-enc_session_key, nonce, tag, ciphertext = \
-   [ file_in.read(x) for x in (private_key.size_in_bytes(), 16, 16, -1) ] # importa la llave AES, texto cifrado y otros parámetros
+      enc_session_key, nonce, tag, ciphertext = \
+         [ file_in.read(x) for x in (private_key.size_in_bytes(), 16, 16, -1) ] # importa la llave AES, texto cifrado y otros parámetros
 
-# Decrypt the session key with the private RSA key
-cipher_rsa = PKCS1_OAEP.new(private_key) #crea un objeto con el cuqal descifra con la llave privada
-session_key = cipher_rsa.decrypt(enc_session_key) #desencripta la llave AES
+      # Decrypt the session key with the private RSA key
+      cipher_rsa = PKCS1_OAEP.new(private_key) #crea un objeto con el cuqal descifra con la llave privada
+      session_key = cipher_rsa.decrypt(enc_session_key) #desencripta la llave AES
 
-# Decrypt the data with the AES session key
-cipher_aes = AES.new(session_key, AES.MODE_EAX, nonce) #crea el objeto para descifrar
-data = cipher_aes.decrypt_and_verify(ciphertext, tag) #Descifra
-#print(data.decode("utf-8")) #Imprime texto descifrado
-file_out.write(data)
-file_out.close()
+      # Decrypt the data with the AES session key
+      cipher_aes = AES.new(session_key, AES.MODE_EAX, nonce) #crea el objeto para descifrar
+      data = cipher_aes.decrypt_and_verify(ciphertext, tag) #Descifra
+      #print(data.decode("utf-8")) #Imprime texto descifrado
+      file_out.write(data)
+      file_out.close()
+      break
+
+   except:
+      print("Error!!")
--- Run_EncryptedData/Prueba_1.py
+++ Run_EncryptedData/Prueba_1.py
@@ -6,7 +6,7 @@
 #
 # GNU Radio Python Flow Graph
 # Title: Not titled yet
-# GNU Radio version: 3.9.5.0
+# GNU Radio version: v3.8.2.0-57-gd71cd177
 
 from distutils.version import StrictVersion
 
@@ -28,23 +28,19 @@ from gnuradio import audio
 from gnuradio import blocks
 import pmt
 from gnuradio import gr
-from gnuradio.fft import window
 import sys
 import signal
 from argparse import ArgumentParser
 from gnuradio.eng_arg import eng_float, intx
 from gnuradio import eng_notation
 from gnuradio.qtgui import Range, RangeWidget
-from PyQt5 import QtCore
-
-
 
 from gnuradio import qtgui
 
 class Prueba_1(gr.top_block, Qt.QWidget):
 
     def __init__(self):
-        gr.top_block.__init__(self, "Not titled yet", catch_exceptions=True)
+        gr.top_block.__init__(self, "Not titled yet")
         Qt.QWidget.__init__(self)
         self.setWindowTitle("Not titled yet")
         qtgui.util.check_set_qss()
@@ -85,14 +81,13 @@ class Prueba_1(gr.top_block, Qt.QWidget):
         # Blocks
         ##################################################
         self._volumen_range = Range(0, 1, 0.05, 0.2, 200)
-        self._volumen_win = RangeWidget(self._volumen_range, self.set_volumen, "'volumen'", "counter_slider", float, QtCore.Qt.Horizontal)
-        self.top_layout.addWidget(self._volumen_win)
+        self._volumen_win = RangeWidget(self._volumen_range, self.set_volumen, 'volumen', "counter_slider", float)
+        self.top_grid_layout.addWidget(self._volumen_win)
         self.qtgui_time_sink_x_0 = qtgui.time_sink_f(
             1024, #size
             samp_rate, #samp_rate
             "", #name
-            1, #number of inputs
-            None # parent
+            1 #number of inputs
         )
         self.qtgui_time_sink_x_0.set_update_time(0.10)
         self.qtgui_time_sink_x_0.set_y_axis(-1, 1)
@@ -133,16 +128,15 @@ class Prueba_1(gr.top_block, Qt.QWidget):
             self.qtgui_time_sink_x_0.set_line_marker(i, markers[i])
             self.qtgui_time_sink_x_0.set_line_alpha(i, alphas[i])
 
-        self._qtgui_time_sink_x_0_win = sip.wrapinstance(self.qtgui_time_sink_x_0.qwidget(), Qt.QWidget)
-        self.top_layout.addWidget(self._qtgui_time_sink_x_0_win)
+        self._qtgui_time_sink_x_0_win = sip.wrapinstance(self.qtgui_time_sink_x_0.pyqwidget(), Qt.QWidget)
+        self.top_grid_layout.addWidget(self._qtgui_time_sink_x_0_win)
         self.qtgui_freq_sink_x_0 = qtgui.freq_sink_f(
             1024, #size
-            window.WIN_BLACKMAN_hARRIS, #wintype
+            firdes.WIN_BLACKMAN_hARRIS, #wintype
             1000, #fc
             samp_rate, #bw
             "", #name
-            1,
-            None # parent
+            1
         )
         self.qtgui_freq_sink_x_0.set_update_time(0.10)
         self.qtgui_freq_sink_x_0.set_y_axis(-140, 10)
@@ -153,7 +147,6 @@ class Prueba_1(gr.top_block, Qt.QWidget):
         self.qtgui_freq_sink_x_0.set_fft_average(1.0)
         self.qtgui_freq_sink_x_0.enable_axis_labels(True)
         self.qtgui_freq_sink_x_0.enable_control_panel(False)
-        self.qtgui_freq_sink_x_0.set_fft_window_normalized(False)
 
 
         self.qtgui_freq_sink_x_0.set_plot_pos_half(not True)
@@ -176,15 +169,16 @@ class Prueba_1(gr.top_block, Qt.QWidget):
             self.qtgui_freq_sink_x_0.set_line_color(i, colors[i])
             self.qtgui_freq_sink_x_0.set_line_alpha(i, alphas[i])
 
-        self._qtgui_freq_sink_x_0_win = sip.wrapinstance(self.qtgui_freq_sink_x_0.qwidget(), Qt.QWidget)
-        self.top_layout.addWidget(self._qtgui_freq_sink_x_0_win)
+        self._qtgui_freq_sink_x_0_win = sip.wrapinstance(self.qtgui_freq_sink_x_0.pyqwidget(), Qt.QWidget)
+        self.top_grid_layout.addWidget(self._qtgui_freq_sink_x_0_win)
         self._freq_range = Range(20, 20000, 10, 500, 200)
-        self._freq_win = RangeWidget(self._freq_range, self.set_freq, "'freq'", "counter_slider", float, QtCore.Qt.Horizontal)
-        self.top_layout.addWidget(self._freq_win)
+        self._freq_win = RangeWidget(self._freq_range, self.set_freq, 'freq', "counter_slider", float)
+        self.top_grid_layout.addWidget(self._freq_win)
         self.blocks_multiply_const_vxx_0 = blocks.multiply_const_ff(volumen)
-        self.blocks_file_source_0 = blocks.file_source(gr.sizeof_float*1, '/home/alex/Documents/ProgrammingProject/MaquinaNativa1/encrypted_data.wav', False, 0, 0)
+        self.blocks_file_source_0 = blocks.file_source(gr.sizeof_float*1, 'C:\\Users\\Julian\\Desktop\\ProgrammingProject\\encrypted_data.bin', False, 0, 0)
         self.blocks_file_source_0.set_begin_tag(pmt.PMT_NIL)
         self.audio_sink_0 = audio.sink(samp_rate, '', True)
+
 
 
         ##################################################
@@ -199,9 +193,6 @@ class Prueba_1(gr.top_block, Qt.QWidget):
     def closeEvent(self, event):
         self.settings = Qt.QSettings("GNU Radio", "Prueba_1")
         self.settings.setValue("geometry", self.saveGeometry())
-        self.stop()
-        self.wait()
-
         event.accept()
 
     def get_volumen(self):
@@ -228,6 +219,7 @@ class Prueba_1(gr.top_block, Qt.QWidget):
 
 
 
+
 def main(top_block_cls=Prueba_1, options=None):
 
     if StrictVersion("4.5.0") <= StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
@@ -242,9 +234,6 @@ def main(top_block_cls=Prueba_1, options=None):
     tb.show()
 
     def sig_handler(sig=None, frame=None):
-        tb.stop()
-        tb.wait()
-
         Qt.QApplication.quit()
 
     signal.signal(signal.SIGINT, sig_handler)
@@ -254,6 +243,11 @@ def main(top_block_cls=Prueba_1, options=None):
     timer.start(500)
     timer.timeout.connect(lambda: None)
 
+    def quitting():
+        tb.stop()
+        tb.wait()
+
+    qapp.aboutToQuit.connect(quitting)
     qapp.exec_()
 
 if __name__ == '__main__':
