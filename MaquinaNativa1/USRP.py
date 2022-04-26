dd5c466cfde3f95d61e0c1e5fec4d026abaf122a
Jorgealza9206 <jaalzar@correo.udistrital.edu.co>
2022-04-26 17:35:43 -0500

    Abril 26 5:35 pm Windows

--- MaquinaNativa1/USRP.py
+++ MaquinaNativa1/USRP.py
@@ -6,7 +6,7 @@
 #
 # GNU Radio Python Flow Graph
 # Title: Not titled yet
-# GNU Radio version: 3.9.5.0
+# GNU Radio version: v3.8.2.0-57-gd71cd177
 
 from distutils.version import StrictVersion
 
@@ -20,29 +20,27 @@ if __name__ == '__main__':
         except:
             print("Warning: failed to XInitThreads()")
 
+from PyQt5 import Qt
+from PyQt5.QtCore import QObject, pyqtSlot
 from gnuradio import analog
 from gnuradio import blocks
 import pmt
 from gnuradio import gr
 from gnuradio.filter import firdes
-from gnuradio.fft import window
 import sys
 import signal
-from PyQt5 import Qt
 from argparse import ArgumentParser
 from gnuradio.eng_arg import eng_float, intx
 from gnuradio import eng_notation
 from gnuradio import uhd
 import time
 
-
-
 from gnuradio import qtgui
 
 class USRP(gr.top_block, Qt.QWidget):
 
     def __init__(self):
-        gr.top_block.__init__(self, "Not titled yet", catch_exceptions=True)
+        gr.top_block.__init__(self, "Not titled yet")
         Qt.QWidget.__init__(self)
         self.setWindowTitle("Not titled yet")
         qtgui.util.check_set_qss()
@@ -76,10 +74,31 @@ class USRP(gr.top_block, Qt.QWidget):
         # Variables
         ##################################################
         self.samp_rate = samp_rate = 96e3
+        self.Selector = Selector = 0
 
         ##################################################
         # Blocks
         ##################################################
+        # Create the options list
+        self._Selector_options = (0, 1, )
+        # Create the labels list
+        self._Selector_labels = ('Ladridos', 'Cancion', )
+        # Create the combo box
+        self._Selector_tool_bar = Qt.QToolBar(self)
+        self._Selector_tool_bar.addWidget(Qt.QLabel('Selector' + ": "))
+        self._Selector_combo_box = Qt.QComboBox()
+        self._Selector_tool_bar.addWidget(self._Selector_combo_box)
+        for _label in self._Selector_labels: self._Selector_combo_box.addItem(_label)
+        self._Selector_callback = lambda i: Qt.QMetaObject.invokeMethod(self._Selector_combo_box, "setCurrentIndex", Qt.Q_ARG("int", self._Selector_options.index(i)))
+        self._Selector_callback(self.Selector)
+        self._Selector_combo_box.currentIndexChanged.connect(
+            lambda i: self.set_Selector(self._Selector_options[i]))
+        # Create the radio buttons
+        self.top_grid_layout.addWidget(self._Selector_tool_bar, 0, 0, 1, 1)
+        for r in range(0, 1):
+            self.top_grid_layout.setRowStretch(r, 1)
+        for c in range(0, 1):
+            self.top_grid_layout.setColumnStretch(c, 1)
         self.uhd_usrp_sink_0 = uhd.usrp_sink(
             ",".join(('', '')),
             uhd.stream_args(
@@ -89,17 +108,20 @@ class USRP(gr.top_block, Qt.QWidget):
             ),
             '',
         )
-        self.uhd_usrp_sink_0.set_samp_rate(samp_rate)
-        # No synchronization enforced.
-
         self.uhd_usrp_sink_0.set_center_freq(70e6, 0)
+        self.uhd_usrp_sink_0.set_gain(0, 0)
         self.uhd_usrp_sink_0.set_antenna('TX/RX', 0)
         self.uhd_usrp_sink_0.set_bandwidth(200e3, 0)
-        self.uhd_usrp_sink_0.set_gain(0, 0)
+        self.uhd_usrp_sink_0.set_samp_rate(samp_rate)
+        # No synchronization enforced.
         self.blocks_short_to_float_0 = blocks.short_to_float(1, 1)
+        self.blocks_selector_0 = blocks.selector(gr.sizeof_short*1,Selector,0)
+        self.blocks_selector_0.set_enabled(True)
         self.blocks_multiply_const_vxx_1 = blocks.multiply_const_cc(1)
         self.blocks_multiply_const_vxx_0 = blocks.multiply_const_ff(32.763e-6)
-        self.blocks_file_source_0 = blocks.file_source(gr.sizeof_short*1, '/home/alex/Documents/ProgrammingProject/MaquinaNativa1/Melendi - Destino o Casualidad ft. Ha Ash VDownloader.wav', True, 0, 0)
+        self.blocks_file_source_0_0 = blocks.file_source(gr.sizeof_short*1, 'D:\\Mi unidad\\ProgrammingProject\\MaquinaNativa1\\Melendi - Destino o Casualidad ft. Ha Ash VDownloader.wav', True, 0, 0)
+        self.blocks_file_source_0_0.set_begin_tag(pmt.PMT_NIL)
+        self.blocks_file_source_0 = blocks.file_source(gr.sizeof_short*1, 'C:\\Users\\Alex\\Desktop\\labrador-barking-daniel_simon.wav', True, 0, 0)
         self.blocks_file_source_0.set_begin_tag(pmt.PMT_NIL)
         self.analog_wfm_tx_0 = analog.wfm_tx(
         	audio_rate=96000,
@@ -110,22 +132,22 @@ class USRP(gr.top_block, Qt.QWidget):
         )
 
 
+
         ##################################################
         # Connections
         ##################################################
         self.connect((self.analog_wfm_tx_0, 0), (self.blocks_multiply_const_vxx_1, 0))
-        self.connect((self.blocks_file_source_0, 0), (self.blocks_short_to_float_0, 0))
+        self.connect((self.blocks_file_source_0, 0), (self.blocks_selector_0, 0))
+        self.connect((self.blocks_file_source_0_0, 0), (self.blocks_selector_0, 1))
         self.connect((self.blocks_multiply_const_vxx_0, 0), (self.analog_wfm_tx_0, 0))
         self.connect((self.blocks_multiply_const_vxx_1, 0), (self.uhd_usrp_sink_0, 0))
+        self.connect((self.blocks_selector_0, 0), (self.blocks_short_to_float_0, 0))
         self.connect((self.blocks_short_to_float_0, 0), (self.blocks_multiply_const_vxx_0, 0))
 
 
     def closeEvent(self, event):
         self.settings = Qt.QSettings("GNU Radio", "USRP")
         self.settings.setValue("geometry", self.saveGeometry())
-        self.stop()
-        self.wait()
-
         event.accept()
 
     def get_samp_rate(self):
@@ -134,6 +156,15 @@ class USRP(gr.top_block, Qt.QWidget):
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
         self.uhd_usrp_sink_0.set_samp_rate(self.samp_rate)
+
+    def get_Selector(self):
+        return self.Selector
+
+    def set_Selector(self, Selector):
+        self.Selector = Selector
+        self._Selector_callback(self.Selector)
+        self.blocks_selector_0.set_input_index(self.Selector)
+
 
 
 
@@ -152,9 +183,6 @@ def main(top_block_cls=USRP, options=None):
     tb.show()
 
     def sig_handler(sig=None, frame=None):
-        tb.stop()
-        tb.wait()
-
         Qt.QApplication.quit()
 
     signal.signal(signal.SIGINT, sig_handler)
@@ -164,6 +192,11 @@ def main(top_block_cls=USRP, options=None):
     timer.start(500)
     timer.timeout.connect(lambda: None)
 
+    def quitting():
+        tb.stop()
+        tb.wait()
+
+    qapp.aboutToQuit.connect(quitting)
     qapp.exec_()
 
 if __name__ == '__main__':
