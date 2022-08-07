9fbf926b4d4ebb1831bd951aec86115e08a29519
Jorgealza9206 <jaalzar@correo.uditrital.edu.co>
2022-08-06 19:29:54 -0500

    Actualizando repositorio

--- MaquinaNativa2/slicer.py
+++ MaquinaNativa2/slicer.py
@@ -1,6 +1,26 @@
-data = "bytes"
+from encodings.utf_8 import decode
 
-with open("encrypted_data_r.bin","rb") as f2:
+index = 0
+data = ""
+data2 = "felix"
+
+with open("encrypted_data.bin","rb") as f2:
     data = f2.read()
 
-print(data)
+#print(data)
+
+for i in data[::-1]:
+    index = index + 1
+    if i != 32:
+        break
+
+print(index)
+
+with open("encrypted_data_r.bin","wb") as f2:
+    f2.write(data[:len(data)-(index-1)])
+
+# print(data)
+
+
+#print(data[::-1])
+
