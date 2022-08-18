f28df2b431e542533ac8bbc1aa7ccb85dbddd9ac
Jorgealza9206 <jaalzar@correo.udistrital.edu.co>
2022-08-18 12:13:44 -0500

    Mejorando capa slicer, concatenador

--- MaquinaNativa1/symmetricalKey.py
+++
@@ -1,9 +0,0 @@
-from Crypto.Random import get_random_bytes
-
-key = get_random_bytes(16)
-print(key)
-
-key_file = open("authen.txt","a")
-key_file.write("\n")
-key_file.write(str(key))
-key_file.close()
