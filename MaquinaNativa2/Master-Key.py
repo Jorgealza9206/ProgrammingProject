c942296dea55753969d7d47b49016860695de3fd
Jorgealza9206 <jaalzar@correo.udistrital.edu.co>
2022-08-18 19:50:28 -0500

    Adecuación

--- MaquinaNativa2/Master-Key.py
+++ MaquinaNativa2/Master-Key.py
@@ -3,7 +3,7 @@ import time
 start = time.time()
 
 exec(open("RSA_KEYS.py").read())
-exec(open("concatenador2.py").read())
+exec(open("filler2.py").read())
 exec(open("PublicKeyTX.py").read())
 
 end = time.time()
