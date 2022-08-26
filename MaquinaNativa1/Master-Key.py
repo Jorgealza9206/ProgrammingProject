e841ebbf9adee6a0bcb8bf73d6975c0849268538
Jorgealza9206 <jaalzar@correo.udistrital.edu.co>
2022-08-26 14:16:23 -0500

    .

--- MaquinaNativa1/Master-Key.py
+++ MaquinaNativa1/Master-Key.py
@@ -1,9 +1,9 @@
-# import time
+import time
 
-# start = time.process_time()
+start = time.process_time()
 
 exec(open("PublicKeyRX.py").read()) #Ejecuta la comunicación
 exec(open("slicer2.py").read())#Quita el relleno de acondicionamiento GNU Radio
 
-# end = time.process_time()
-# delay = end - start
+end = time.process_time()
+delay = end - start
